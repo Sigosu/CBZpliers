31b2df82ab96fa5798f0252acfdc0775f81c71a9
Sigosu <szymon.sciegienka@gmail.com>
2024-09-23 19:15:25 +0200

    Added a new argument

--- src/main.py
+++ src/main.py
@@ -8,9 +8,10 @@ def main():
     parser.add_argument("cbz_dir", type=Path, help="Path to the folder containing .cbz files")
     parser.add_argument("volume_title", help="Title for the output .cbz file")
     parser.add_argument("series_title", help="Title of the manga series, only shows in the .xml file, but could be useful on some tablets")
-
+    parser.add_argument("-m", help="Use this flag if you want to run the script for multiple directories at once. It runs the script for EVERY directory given in cbz_dir argument", action="store_true", required=False)
     args = parser.parse_args()
-    combine_files(args.cbz_dir, args.volume_title, args.series_title)
+    print(args)
+    combine_files(args.cbz_dir, args.volume_title, args.series_title, args.m)
 
 if __name__ == "__main__":
     main()
