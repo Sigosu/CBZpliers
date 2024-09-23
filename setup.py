9d58a82cdd632be5a4f7b491a08db41f7524ed79
Sigosu <szymon.sciegienka@gmail.com>
2024-09-23 19:16:40 +0200

    Fixed GitHub url, new version

--- setup.py
+++ setup.py
@@ -2,15 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name="CBZpliers",
-    version="0.1.0",
+    version="0.2.0",
     packages=find_packages(),
     install_requires=[
-        "lxml",          # List of dependencies
-        "setuptools",
+        "lxml",
     ],
     entry_points={
         'console_scripts': [
-            'combine_cbz=src.main:main',  # Command-line tool configuration
+            'combine_cbz=src.main:main',
         ],
     },
     author="Szymon Sciegienka",
@@ -18,10 +17,10 @@ setup(
     description="A tool to combine multiple .cbz files into one.",
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/yourusername/pliers",  # Optional GitHub link
+    url="https://github.com/Sigosu/CBZpliers",
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",   # Adjust based on your license
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',  # Minimum Python version required
